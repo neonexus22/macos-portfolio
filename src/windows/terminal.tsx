e7c400871cfb4ff58a74ccc63c8468af42e46481
nishant-shah <112845053+nishant-spendflo@users.noreply.github.com>
2025-12-14 17:41:37 +0545

    feat/window-wrapper 1. implement a hoc for managing window states 2. added terminal window

---
+++ src/windows/terminal.tsx
@@ -0,0 +1,56 @@
+/** biome-ignore-all lint/suspicious/noArrayIndexKey: <explanation> */
+import { Check, CheckIcon, Flag } from "lucide-react"
+import { techStack } from "../constants"
+import WindowWrapper from "../hoc/window-wrapper"
+import WindowControls from "../components/window-controls"
+
+
+const Terminal = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="terminal" />
+                <h2>Tech Stack</h2>
+            </div>
+            <div className="techstack">
+                <p>
+                    <span className="font-bold">@nishnat % </span>
+                    show tech stack
+                </p>
+                <div className="label">
+                    <p className="w-32">Category</p>
+                    <p>Technologies</p>
+                </div>
+
+                <ul className="content">
+                    {techStack.map(({ category, items }) => (
+                        <li key={category} className="flex items-center">
+                            <Check className="check" size={20} />
+                            <h3>{category}</h3>
+                            <ul>
+                                {items.map((item, i) => (
+                                    <li key={i}>
+                                        {item}{i < items.length - 1 ? "," : ""}
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+                <div className="footnote">
+                    <p>
+                        <CheckIcon size={20} /> 5 0f 5 stacks loaded successfully (100%)
+                    </p>
+                    <p className="text-black">
+                        <Flag size={15} fill="black" />
+                        Render time: 5ms
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const TerminalWindow = WindowWrapper(Terminal, 'terminal')
+
+export default TerminalWindow
